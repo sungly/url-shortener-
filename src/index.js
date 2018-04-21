3dffbb88299b33a7ac4f20a8eec36862afa4cb88
sungly <sungly.77@gmail.com>
2018-04-21 17:48:56 -0400

    add

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ const app = express();
 
 app.get("/", (req, res) => res.send("url shortener set up!"));
 
+app.post("/api/encodeUrl", (req, res) => {
+  // return a shorten version of the original url
+});
+
+app.get("/api/decodeUrl", (req, res) => {
+  // get the original url from the shorten url
+});
+
 app.listen(8080, () => console.log("Server starting on server 8080..."));
